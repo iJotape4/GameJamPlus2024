61fbc8d9a3022a881b057a091a3bf9d09e866af2
Juan <jcamacho249@unab.edu.co>
2024-10-18 20:30:01 -0500

    [Add] Movement Input + [Fix] Camera location
    + Movement with WASD
    + Characters had W and D input inverted cuz of the camera location

--- GameJamPlus2024/Source/GameJamPlus2024/Private/PaperCharacterBase.cpp
+++ GameJamPlus2024/Source/GameJamPlus2024/Private/PaperCharacterBase.cpp
@@ -3,6 +3,62 @@
 
 #include "GameJamPlus2024/Public/PaperCharacterBase.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameJamPlus2024/GameJamPlus2024Character.h"
+
 APaperCharacterBase::APaperCharacterBase()
 {
+}
+
+
+void APaperCharacterBase::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APaperCharacterBase::Move);
+	}
+	else
+	{
+		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void APaperCharacterBase::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void APaperCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
 }
--- GameJamPlus2024/Source/GameJamPlus2024/Public/PaperCharacterBase.h
+++ GameJamPlus2024/Source/GameJamPlus2024/Public/PaperCharacterBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
+#include "GameJamPlus2024/GameJamPlus2024Character.h"
 #include "PaperCharacterBase.generated.h"
 
 /**
@@ -14,7 +15,25 @@ class GAMEJAMPLUS2024_API APaperCharacterBase : public APaperCharacter
 {
 	GENERATED_BODY()
 
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+	
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+	
 public:
 	APaperCharacterBase();
+
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
+	// To add mapping context
+	virtual void BeginPlay();
 };
