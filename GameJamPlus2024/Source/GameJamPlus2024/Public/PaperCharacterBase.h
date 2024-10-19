04e866caaf72707724ee5be6d961f1aa134cb7a1
Juan <jcamacho249@unab.edu.co>
2024-10-18 19:44:35 -0500

    [Add] PaperCharacter Base Class

---
+++ GameJamPlus2024/Source/GameJamPlus2024/Public/PaperCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAMPLUS2024_API APaperCharacterBase : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
